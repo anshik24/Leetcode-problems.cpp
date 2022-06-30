291bae151a1369b2e61068a655fb68b3b3302ce4
Anshika Singh <99993107+anshik24@users.noreply.github.com>
2022-07-01 00:21:38 +0530

    validate BST

---
+++ validateBST.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    bool isBST(TreeNode* root,TreeNode*min,TreeNode*max){
+                if(root==NULL){
+            return true;
+
+        }
+        if(min!= NULL && root->val<=min->val){
+            return false;
+        }
+                if(max!= NULL && root->val >=max-> val){
+            return false;
+        }
+        return isBST(root->left,min,root) && isBST(root->right,root,max);
+    
+
+        
+    }
+    bool isValidBST(TreeNode* root) {
+
+      return isBST(root,NULL,NULL);
+    }
+};
